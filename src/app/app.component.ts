096b40df4fedc5b31c78dec8ddc43e4f17c89720
arsany98 <arsanymaged13@gmail.com>
2025-09-26 20:52:16 +0300

    Electron integration and coptic transliteration UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { tranliterate } from '../../.yalc/coptic-arabic-transliterate';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'arabized-coptic';
+  copticText = '';
+  arabicText = '';
+  value = 'clear me';
+  onCopticInput() {
+    this.arabicText = tranliterate(this.copticText);
+  }
 }
