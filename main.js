08267f3456c1d39c07f3364f7e03f8bc102d70da
arsany98 <arsanymaged13@gmail.com>
2025-11-22 02:20:58 +0200

    Change App Name

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 // main.js
-const { app, BrowserWindow, Menu } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
 
 // Error Handling
-process.on('uncaughtException', (error) => {
-    console.error("Unexpected error: ", error);
+process.on("uncaughtException", (error) => {
+  console.error("Unexpected error: ", error);
 });
 function createWindow() {
-    Menu.setApplicationMenu(null);
-    // const win = new BrowserWindow({
-    //     width: 800,
-    //     height: 600,
-    //     webPreferences: {
-    //         preload: path.join(__dirname, 'preload.js'),
-    //         contextIsolation: true,
-    //         enableRemoteModule: false,
-    //     }
-    // });
-    // win.loadURL(
-    //     url.format({
-    //         pathname: path.join(__dirname, `/dist/arabized-coptic/browser/index.html`),
-    //         protocol: "file:",
-    //         slashes: true,
-    //     })
-    // );
-  win = new BrowserWindow({width: 800, height: 600});
-  win.loadFile('dist/arabized-coptic/browser/index.html');
-
+  Menu.setApplicationMenu(null);
+  // const win = new BrowserWindow({
+  //     width: 800,
+  //     height: 600,
+  //     webPreferences: {
+  //         preload: path.join(__dirname, 'preload.js'),
+  //         contextIsolation: true,
+  //         enableRemoteModule: false,
+  //     }
+  // });
+  // win.loadURL(
+  //     url.format({
+  //         pathname: path.join(__dirname, `/dist/coptx/browser/index.html`),
+  //         protocol: "file:",
+  //         slashes: true,
+  //     })
+  // );
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile("dist/coptx/browser/index.html");
 }
 // App Lifecycle
 app.whenReady().then(createWindow);
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
